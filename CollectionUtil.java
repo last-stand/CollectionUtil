f67b61723ee62a5f8f4bc2de2b0151adef5fe1b8
Jai Prakash <jaiprak@thoughtworks.com>
2015-03-02 09:59:06 +0530

    CollectionUtil version 1 : is completed.

--- CollectionUtil.java
+++ CollectionUtil.java
@@ -1,11 +1,20 @@
 import java.util.*;
 
 public class CollectionUtil{
-	public static<E> List<E> map(List<E> dataList, ListMapper lmap){
-		List<E> result = new ArrayList();
-		for (E data : dataList) {
-			E element = (E)lmap.mapper(data,0,dataList);
+	public static<E> List<E> map(List<E> dataList, ListMapper<E> lmap){
+		List<E> result = new ArrayList<E>();
+		for(int i=0; i<dataList.size(); i++){
+			E element = lmap.mapper(dataList.get(i),i,dataList);
 			result.add(element);
+		}
+		return result;
+	}
+
+	public static<E> List<E> filter(List<E> dataList, ListFilter<E> lfilter){
+		List<E> result = new ArrayList<E>();
+		for(int i=0; i<dataList.size(); i++){
+			if(lfilter.filterFunc(dataList.get(i),i,dataList))
+				result.add(dataList.get(i));
 		}
 		return result;
 	}
--- CollectionUtilTest.java
+++ CollectionUtilTest.java
@@ -6,6 +6,10 @@ interface ListMapper<E> {
     E mapper(E element, int index, List<E> dataList);
 }
 
+interface ListFilter<E>{
+    boolean filterFunc(E element, int index, List<E> dataList);
+}
+
 class IntMapper implements ListMapper<Integer> {
     public Integer mapper(Integer element, int index, List<Integer> dataList){
         return element*element*element;
@@ -18,11 +22,22 @@ class StringMapper implements ListMapper<String> {
     }
 }
 
+class IntFilter implements ListFilter<Integer> {
+    public boolean filterFunc(Integer element, int index, List<Integer> dataList){
+        return element%2 == 0;
+    }
+}
+
+class StringFilter implements ListFilter<String> {
+    public boolean filterFunc(String element, int index, List<String> dataList){
+        return element.length() > 4;
+    }
+}
 
 public class CollectionUtilTest {
     @Test
     public void map_returns_upperCase_of_all_elements_of_given_STRING_LIST(){
-        ListMapper mapper = new StringMapper();
+        ListMapper<String> mapper = new StringMapper();
         List<String> str = new ArrayList<String>();
         List<String> expected = new ArrayList<String>();
         str.add("Hello");
@@ -37,10 +52,10 @@ public class CollectionUtilTest {
         assertEquals(expected.get(1),result.get(1));
         assertEquals(expected.get(2),result.get(2));
     }
-    
+
     @Test
     public void map_returns_cube_of_all_elements_of_given_INTEGER_LIST(){
-        ListMapper mapper = new IntMapper();
+        ListMapper<Integer> mapper = new IntMapper();
         List<Integer> numbers = new ArrayList<Integer>();
         List<Integer> expected = new ArrayList<Integer>();
         numbers.add(1);
@@ -54,5 +69,39 @@ public class CollectionUtilTest {
         assertEquals(expected.get(0),result.get(0));
         assertEquals(expected.get(1),result.get(1));
         assertEquals(expected.get(2),result.get(2));
+    }
+
+    @Test
+    public void filter_returns_all_even_numbers_from_given_INTEGER_LIST(){
+        ListFilter<Integer> lfilter = new IntFilter();
+        List<Integer> numbers = new ArrayList<Integer>();
+        List<Integer> expected = new ArrayList<Integer>();
+        numbers.add(11);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(88);
+
+        expected.add(2);
+        expected.add(88);
+        List<Integer> result = CollectionUtil.<Integer>filter(numbers,lfilter);
+        assertEquals(expected.get(0),result.get(0));
+        assertEquals(expected.get(1),result.get(1));
+    }
+
+     @Test
+    public void filter_returns_list_of_string_whose_length_greater_than_4_from_given_String_LIST(){
+        ListFilter<String> lfilter = new StringFilter();
+        List<String> str = new ArrayList<String>();
+        List<String> expected = new ArrayList<String>();
+        str.add("Jai");
+        str.add("India");
+        str.add("Bharat");
+        str.add("Hind");
+
+        expected.add("India");
+        expected.add("Bharat");
+        List<String> result = CollectionUtil.<String>filter(str,lfilter);
+        assertEquals(expected.get(0),result.get(0));
+        assertEquals(expected.get(1),result.get(1));
     }
 }
